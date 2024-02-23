f44c1f080fb3ad932afdd1de2db26ab501b0e728
Alain Guillon <zyrass@outlook.fr>
2024-02-23 07:05:16 +0100

    Ajout des commentaires pour la partie programme

--- QuizzCapitales/Program.cs
+++ QuizzCapitales/Program.cs
@@ -4,6 +4,11 @@
   {
     static void Main(string[] args)
     {
+      // Première version du jeu : toutes les questions sont posés.
+      // Quizz.Jouer();
+
+      // Seconde version du jeu : N'est posé que les questions passé en paramètre.
+      // Les numéros invalide seront ignoré dans le jeu
       Quizz.Jouer(12, 5, 18, 31, 3);
     }
   }
